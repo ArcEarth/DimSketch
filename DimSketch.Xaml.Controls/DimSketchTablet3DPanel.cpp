b0d53172016f746245de47eb3dbdee9951bc9aea
Arc Earth <ArcEarth@outlook.com>
2015-06-05 18:32:32 -0400

    Initial Commit
    Setup the C# and C++ project

---
+++ DimSketch.Xaml.Controls/DimSketchTablet3DPanel.cpp
@@ -0,0 +1,63 @@
+﻿#include "pch.h"
+#include "DimSketchTablet3DPanel.h"
+#include "DirectXHelper.h"
+#include <DirectXColors.h>
+
+using namespace DimSketch::Xaml::Controls::Base;
+
+using namespace Concurrency;
+using namespace DirectX;
+using namespace Windows::Foundation;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Data;
+
+DimSketch::Xaml::Controls::Tablet3DViewPanel::Tablet3DViewPanel()
+	: Panel()
+{
+	
+}
+
+void DimSketch::Xaml::Controls::Tablet3DViewPanel::NotifyPropertyChanged(Platform::String ^ prop)
+{
+
+}
+
+void DimSketch::Xaml::Controls::Tablet3DViewPanel::Update(double elapsedTime)
+{
+	//Panel::Update(elapsedTime);
+}
+
+void DimSketch::Xaml::Controls::Tablet3DViewPanel::Render()
+{
+	BeginRender(nullptr, nullptr, DirectX::Colors::Azure.f);
+	EndRender();
+}
+
+void DimSketch::Xaml::Controls::Tablet3DViewPanel::ResetDeviceResources()
+{
+	Panel::ResetDeviceResources();
+}
+
+void DimSketch::Xaml::Controls::Tablet3DViewPanel::CreateDeviceResources()
+{
+	Panel::CreateDeviceResources();
+}
+
+void DimSketch::Xaml::Controls::Tablet3DViewPanel::CreateSizeDependentResources()
+{
+	Panel::CreateSizeDependentResources();
+}
+
+void DimSketch::Xaml::Controls::Tablet3DViewPanel::StartRenderLoop()
+{
+	Panel::StartRenderLoop();
+}
+
+void DimSketch::Xaml::Controls::Tablet3DViewPanel::StopRenderLoop()
+{
+	Panel::StopRenderLoop();
+}
+
+DimSketch::Xaml::Controls::Tablet3DViewPanel::~Tablet3DViewPanel()
+{
+}
---
+++ DimSketch.Xaml.Controls/DimSketchTablet3DPanel.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "Panel.h"
+
+namespace DimSketch {
+	namespace Xaml {
+		namespace Controls {
+				using namespace DimSketch::Xaml::Controls::Base;
+
+				public ref class Tablet3DViewPanel sealed
+					: public Panel
+				{
+				public:
+					Tablet3DViewPanel();
+
+				protected private:
+					virtual event Windows::UI::Xaml::Data::PropertyChangedEventHandler^ PropertyChanged;
+					void NotifyPropertyChanged(Platform::String^ prop);
+
+					virtual void Update(double elapsedTime) override;
+					virtual void Render() override;
+
+					virtual void ResetDeviceResources() override;
+					virtual void CreateDeviceResources() override;
+					virtual void CreateSizeDependentResources() override;
+
+					// override to add mouse/touch/pen input
+					virtual void StartRenderLoop() override;
+					virtual void StopRenderLoop() override;
+
+				private:
+					~Tablet3DViewPanel();
+
+				private:
+
+				};
+		}
+	}
+}
